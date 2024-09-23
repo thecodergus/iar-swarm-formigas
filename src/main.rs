60dc9f7be86308e91eed091b57bee615b2fc517c
PC do função que tem rx 550 <gustavo.michels.de.camargo@gmail.com>
2024-09-23 12:58:24 -0300

    Acertando hiper parametros

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use lib::formiga::{gerar_formigas, Formiga};
 use lib::grao::gerar_graos;
 
 fn main() {
-    let tamanho: (f64, f64) = (800.0, 600.0);
+    let tamanho: (f64, f64) = (64.0, 64.0);
     let tamanho_mapa: (f64, f64) = tamanho.clone();
     
     // Criar grãos aleatórios igual a 0,01% do tamanho da matriz
-    let quantidade_graos = (tamanho.0 * tamanho.1 * 0.001) as i32;
+    let quantidade_graos = 400;
+    let quantidade_formigas = 100;
 
-    let mut cenario: Cenario = Cenario::new(tamanho, gerar_formigas(20, tamanho_mapa), gerar_graos(quantidade_graos, tamanho_mapa));
+    let mut cenario: Cenario = Cenario::new(tamanho, gerar_formigas(quantidade_formigas, tamanho_mapa), gerar_graos(quantidade_graos, tamanho_mapa));
     cenario.start(500_000_000)
 }
