4ac460dd53b6d3707557d3682b1616abcf186f62
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-10-03 16:07:36 -0300

    Copiando a formula do artigo

--- src/lib/constantes.rs
+++ src/lib/constantes.rs
@@ -1,7 +1,7 @@
 // Parametros
-pub const TAMANHO_VIZINHANCA: i32 = 2; // Definindo o tamanho da vizinhança (a distância máxima em cada direção)
-pub const ALPHA: f64 = 0.1;
-pub const K1: f64 = 0.05;
-pub const K2: f64 = 0.9;
-pub const NUM_FORMIGAS: i32 = 20;
+pub const TAMANHO_VIZINHANCA: i32 = 1; // Definindo o tamanho da vizinhança (a distância máxima em cada direção)
+pub const ALPHA: f64 = 12.0;
+pub const K1: f64 = 0.5;
+pub const K2: f64 = 0.5;
+pub const NUM_FORMIGAS: i32 = 100;
 pub const NUM_INTERACOES: i64 = 4_000_000;
