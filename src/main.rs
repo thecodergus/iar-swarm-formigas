3238669a0877c4613fd0eae3b37c764ba9237809
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-10-03 15:36:43 -0300

    Melhorando uns negocio

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod lib;
 use lib::cenario::Cenario;
+use lib::constantes::{NUM_FORMIGAS, NUM_INTERACOES};
 use lib::formiga::{gerar_formigas, Formiga};
 use lib::grao::{gerar_graos, ler_graos_de_arquivo, ler_graos_de_arquivo_normalizado};
 
@@ -8,12 +9,12 @@ fn main() {
     let tamanho_mapa: (f64, f64) = tamanho.clone();
 
     // Criar grãos aleatórios igual a 0,01% do tamanho da matriz
-    let quantidade_formigas = 100;
+    let quantidade_formigas: i32 = NUM_FORMIGAS;
 
     let mut cenario: Cenario = Cenario::new(
         tamanho,
         gerar_formigas(quantidade_formigas, tamanho_mapa),
         ler_graos_de_arquivo("Square1-DataSet-400itens.txt", tamanho_mapa).unwrap(),
     );
-    cenario.start(50_000_000)
+    cenario.start(NUM_INTERACOES)
 }
